6e884197f33a34a883fba690a03033bf30b4cc48
maxmeldal <maxmeldal@gmail.com>
2021-12-01 15:19:34 +0100

    audio and video get create and save made for attractions

--- app/src/main/java/com/example/christiansoeappproject/model/Attraction.java
+++ app/src/main/java/com/example/christiansoeappproject/model/Attraction.java
@@ -1,11 +1,74 @@
 package com.example.christiansoeappproject.model;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Base64;
+
+
 public class Attraction extends Location {
+
+    @SerializedName("video")
+    private String video;
+
+    @SerializedName("audio")
+    private String audio;
+
+    public Attraction(String id, double latitude, double longitude, String name, String video, String audio) {
+        super(id, latitude, longitude, name);
+        this.video = video;
+        this.audio = audio;
+    }
+
+    public Attraction(double latitude, double longitude, String name, String video, String audio) {
+        super(latitude, longitude, name);
+        this.video = video;
+        this.audio = audio;
+    }
+
+
     public Attraction(String id, double latitude, double longitude, String name) {
         super(id, latitude, longitude, name);
     }
 
     public Attraction(double latitude, double longitude, String name) {
         super(latitude, longitude, name);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public byte[] getVideo() {
+        if (video != null) {
+            return Base64.getDecoder().decode(video.getBytes());
+        }
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void setVideo(byte[] video) {
+        this.video = Base64.getEncoder().encodeToString(video);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public byte[] getAudio() {
+        if (audio != null) {
+            return Base64.getDecoder().decode(audio.getBytes());
+        }
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void setAudio(byte[] audio) {
+        this.audio = Base64.getEncoder().encodeToString(audio);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Attraction{" +
+                "video='" + video + '\'' +
+                ", audio='" + audio + '\'' +
+                '}';
     }
 }
