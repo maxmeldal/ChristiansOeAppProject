f6711fcacf3ef80830e0f438fe516391adf94829
nicolaiokkels <nico902t@stud.kea.dk>
2021-12-01 13:34:41 +0100

    added themes images

---
+++ app/src/main/java/com/example/christiansoeappproject/ThemeActivity.java
@@ -0,0 +1,22 @@
+package com.example.christiansoeappproject;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThemeActivity extends AppCompatActivity {
+
+    GridView gridView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_dashboard);
+
+        //gridView = findViewById(R.id.gridView);
+
+
+    }
+}
