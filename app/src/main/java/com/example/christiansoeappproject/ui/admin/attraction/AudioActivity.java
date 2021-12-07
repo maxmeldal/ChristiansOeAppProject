ba4fb2f9951901ddd35338844d44723ebea70958
maxmeldal <maxmeldal@gmail.com>
2021-12-07 13:04:21 +0100

    record audio button added

--- app/src/main/java/com/example/christiansoeappproject/ui/admin/attraction/AudioActivity.java
+++ app/src/main/java/com/example/christiansoeappproject/ui/admin/attraction/AudioActivity.java
@@ -1,9 +1,12 @@
 package com.example.christiansoeappproject.ui.admin.attraction;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
@@ -22,35 +25,43 @@ public class AudioActivity extends AppCompatActivity {
     private byte[] audio;
 
     public void start(View view){
-        ParcelFileDescriptor[] descriptors = new ParcelFileDescriptor[0];
-        try {
-            descriptors = ParcelFileDescriptor.createPipe();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        ParcelFileDescriptor parcelRead = new ParcelFileDescriptor(descriptors[0]);
-        ParcelFileDescriptor parcelWrite = new ParcelFileDescriptor(descriptors[1]);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
 
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        recorder.setOutputFile(parcelWrite.getFileDescriptor());
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
 
-        InputStream inputStream = new ParcelFileDescriptor.AutoCloseInputStream(parcelRead);
-        try {
-            recorder.prepare();
-            recorder.start();
+        } else {
 
-            int read;
-            byte[] data = new byte[16384];
-
-            while ((read = inputStream.read(data, 0, data.length)) != -1){
-                byteArrayOutputStream.write(data,0, read);
+            ParcelFileDescriptor[] descriptors = new ParcelFileDescriptor[0];
+            try {
+                descriptors = ParcelFileDescriptor.createPipe();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
             }
-            audio= byteArrayOutputStream.toByteArray();
 
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+            ParcelFileDescriptor parcelRead = new ParcelFileDescriptor(descriptors[0]);
+            ParcelFileDescriptor parcelWrite = new ParcelFileDescriptor(descriptors[1]);
+
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            recorder.setOutputFile(parcelWrite.getFileDescriptor());
+
+            InputStream inputStream = new ParcelFileDescriptor.AutoCloseInputStream(parcelRead);
+            try {
+                recorder.prepare();
+                recorder.start();
+
+                int read;
+                byte[] data = new byte[16384];
+
+                while ((read = inputStream.read(data, 0, data.length)) != -1) {
+                    byteArrayOutputStream.write(data, 0, read);
+                }
+                audio = byteArrayOutputStream.toByteArray();
+
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
